538a8722bf33912f7fb2039681c36a64cabcd7c3
Phenol Lully <lullyp95@gmail.com>
2024-03-26 22:10:45 -0400

    Added README

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
+
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
   await server.start();
